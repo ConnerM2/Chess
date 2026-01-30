4664193aa04fc00d4cf95e900b1dd89623b43997
ConnerM Windows <manwillconner3@gmail.com>
2026-01-29 22:12:36 -0700

    Can now move the peices around and capture other peices. Legal moves are not accounted for yet so they can move anywhere.
    Moves also get stored in a list so to be added is a undo button to undo moves

--- chess_Engine.py
+++ chess_Engine.py
@@ -14,3 +14,20 @@ class GameState():
         
         self.whiteToMove = True
         self.moveHistory = []
+
+    def makeMove(self, move):
+        self.board[move.startCol][move.startRow] = '--'
+        self.board[move.endCol][move.endRow] = move.peice_moved
+        self.moveHistory.append(move)
+        print(move)
+        self.whiteToMove = not self.whiteToMove
+
+class Move():
+    def __init__(self, startSQ, endSQ, board):
+        self.startCol = startSQ[0]
+        self.startRow = startSQ[1]
+        self.endCol = endSQ[0]
+        self.endRow = endSQ[1]
+        self.peice_moved = board[self.startCol][self.startRow]
+        self.peice_captured = board[self.endCol][self.endRow]
+        print(self.peice_moved, self.peice_captured)
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pygame as p
-from chess_Engine import GameState
+from chess_Engine import GameState, Move
 import os
 
 WIDTH = HEIGHT = 512
-DIMENSIONS = 8 
-SQUARE_SIZE = HEIGHT // DIMENSIONS# 64 
-BASE_DIRECTORY = os.path.dirname(os.path.abspath(__file__)) #get complete path from C drive to main file. This goes back and get main directory, this case "chess"
-print(f"This is abls path {os.path.abspath(__file__)}")
-print(f"This is the base directory {BASE_DIRECTORY}")
+DIMENSIONS = 8 #8X8 board
+SQUARE_SIZE = HEIGHT // DIMENSIONS# 64 pixel per square
+BASE_DIRECTORY = os.path.dirname(os.path.abspath(__file__)) #get complete path from C drive to main file. Then go trims and returns base dir, this case "chess"
 IMAGES = {}
 WHITE = (237, 214, 176)
 BROWN = (184, 135, 98)
-
+print(f"This is abls path {os.path.abspath(__file__)}")
+print(f"This is the base directory {BASE_DIRECTORY}")
+gs = GameState()
 
 def load_images():
     #makes a list of all the possible chess peices
@@ -24,25 +24,44 @@ def load_images():
         #this assigns an images to each peice
         IMAGES[peice] = p.transform.scale(p.image.load(path), (SQUARE_SIZE, SQUARE_SIZE))
 
-load_images()
-        
-gs = GameState()
-running = True
-
 def main():
     p.init()
+    load_images()
     clock = p.time.Clock()
     screen = p.display.set_mode((WIDTH, HEIGHT)) #
+    running = True
+    moves = []
     while running:
+
         for event in p.event.get():
             if event.type == p.QUIT:
                 p.quit()
                 exit()
+            elif event.type == p.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    position = event.pos #Gets position of mouse once clicked, store in position as a tuple
+                    column = position[0] // SQUARE_SIZE #pos[0] = x pos[1] = y; divide each by 64 to get row/column of mouse click
+                    row = position[1] // SQUARE_SIZE
+                    move = (row, column) #store move in tuple
+                    moves.append(move) 
+                    
+                    if len(moves) == 2: #waits till two moves are made
+                        if move == moves[0]: #if click came square, nothing happens
+                            moves = []
+                            print("Same square")
+                        else:
+                            print(f"Row: {row} Column: {column} Move: {moves}")
+                            move = Move(moves[0], moves[1], gs.board) #sends the two squares clicked to backend
+                            gs.makeMove(move)
+                            moves = []
+                    
 
         p.display.update()
         game_start(screen)
         clock.tick(16)
         p.display.flip()
+
+
 
 def game_start(screen):
     draw_board(screen)
@@ -50,19 +69,19 @@ def game_start(screen):
 
 def draw_board(screen):
     for row in range(DIMENSIONS):
-        for column in range(DIMENSIONS):
-            if (row + column) % 2 == 0:
+        for column in range(DIMENSIONS): #8x8 loop to draw all 64 square
+            if (row + column) % 2 == 0: 
                 color = WHITE
             else:
                 color = BROWN
             p.draw.rect(screen, color, p.Rect(row * SQUARE_SIZE, column * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            #draws rec on screen with color. Useing row and column loop, get cords for square: (0,0), (0,64), (0,128)
 
 def draw_peices(screen, board):
     for row in range(DIMENSIONS):
-        for column in range(DIMENSIONS):
-            if board[row][column] != '--':
-                screen.blit(IMAGES[board[row][column]], (column * SQUARE_SIZE, row * SQUARE_SIZE))
-
+        for column in range(DIMENSIONS): #Do a 8x8 loop to go over every square on board
+            if board[row][column] != '--': #Use the board attribute from GameState to see if a peice is needed or --
+                screen.blit(IMAGES[board[row][column]], (column * SQUARE_SIZE, row * SQUARE_SIZE)) #Using the keys from the dic, blit the peice images onto the square
 
 
 if __name__ == '__main__':
